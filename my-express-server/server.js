14d44d0f457132162308758efd1a7c28674985a0
Jonel Dominic Tapang <jtapang@fullscale.io>
2023-01-29 00:06:13 +0800

    my-express-server changes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Logged in!");
 });
 
 app.get("/contact", (req, res) => {
